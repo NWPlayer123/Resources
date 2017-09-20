86af44df9e1cc8f6805be68646772804a4a60960
NWPlayer123 <NWPlayer123@users.noreply.github.com>
2017-09-19 20:59:56 -0600

    Pikmin 1 files part 1

---
+++ GameCube/GPIE01/src/sysDolphin/texture.cpp
@@ -0,0 +1,35 @@
+Texture::Texture(void) {
+	
+}
+
+Texture::getAlpha(int, int) {
+	
+}
+
+Texture::read(RandomAccessStream &) {
+	
+}
+
+Texture::detach(void) {
+	
+}
+
+Texture::attach(void) {
+	
+}
+
+Texture::createBuffer(int, int, int, void *) {
+	
+}
+
+Texture::grabBuffer(int, int, bool, bool) {
+	
+}
+
+Texture::decodeData(TexImg *) {
+	
+}
+
+void Texture::makeResident(void) {
+	return; //stubbed
+}
